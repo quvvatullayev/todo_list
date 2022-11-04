ef4e1720307646d1ef44d47ce184287197148552
tajriba <quvvatullayev@gmail.com>
2022-11-04 10:08:06 +0300

    craete todo list

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse,JsonResponse
+from .models import Todo_list_model
 
 # Create your views here.
+class Create_todolist(View):
+    def post(self, request):
+        r = request.POST
+        product = Todo_list_model()
+        product.task = r.get('task')
+        product.status = r.get('status')
+        product.save()
+        return JsonResponse(product)
