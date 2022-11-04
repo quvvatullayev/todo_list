a31ef30693f4adc40215a979ea4acae2ef583d80
tajriba <quvvatullayev@gmail.com>
2022-11-04 09:40:38 +0300

    create project models

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Todo_list_model
 
 # Register your models here.
+admin.site.register(Todo_list_model)
--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Todo_list_model(models.Model):
+    task = models.TextField()
+    status = models.TextField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
